1a8bb24b990215074bc22a4f947415a84d877026
josephgaleano <joseph.galeano@galeanoea.com>
2024-11-13 12:30:54 -0600

    Mejorar documentación

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 /**
+ * @license
+ * Copyright (c) 2024 Galeano Engineering & Architecture.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
  * Valor del periodo estructural  que limita la parte ascendente del espectro de diseño
  * @constant
  * @type {number}
@@ -51,7 +74,7 @@ export function correcionA0(a0, grupo) {
  * @param {string} suelo Tipo de suelo
  * @return {number}
  */
-export function calcularS(zona, suelo) {
+export function S(zona, suelo) {
   // NOTE para suelos de tipo IV es necesario realizar espectros de diseño de sitio especificio
   let S = 0;
   switch (suelo) {
@@ -65,7 +88,7 @@ export function calcularS(zona, suelo) {
       if (zona === "B") S = 1.7;
       if (zona === "C") S = 1.5;
       break;
-    // Para suelos tipo I el valor de S siempre es 1, no importa la zona sismica
+    // Para suelos tipo I el valor de S siempre es 1, no importa la zona sísmica
     default:
       S = 1;
       break;
@@ -75,11 +98,11 @@ export function calcularS(zona, suelo) {
 }
 
 /**
- * Calcular Factor de Reducción por comportamiento dúctil de una estructura (Q1)
+ * Calcular el Factor de Reducción por comportamiento dúctil de una estructura (Q1)
  * @param {number} Q Capacidad dúctil de una estructura
  * @return {number}
  */
-export function calcularQ1(Q) {
+export function Q1(Q) {
   // if (T <= Ta) {
   //   Q1 = 1 + (T / Ta) * (Q - 1);
   // }
@@ -103,12 +126,12 @@ export function correccionIrregularidad(Q1, FI) {
  * @param {number} a0 Aceleración máxima del terreno
  * @return {number}
  */
-export function calcularD(a0) {
+export function d(a0) {
   return 2.7 * a0;
 }
 
 /**
- * Cálcula la reducción del espectro para el punto dado
+ * Calcular la reducción del espectro para el punto dado
  * @param {number} val Valor del espectro elástico para el punto (T) actual
  * @param {number} Q1 Factor de reducción por comportamiento dúctil de una estructura
  * @return {number}
@@ -162,33 +185,3 @@ export function generarEspectro(a0, S, d, Q1Corregido, periodo, intervalo) {
 
   return { elastico, reducido };
 }
-
-// NOTE float? 4-6 decimales
-
-// I will make an easy calculation of the required point
-// so:
-//    point < Ta
-//    Ta < point < tb
-//    Tb < point < Tc
-//    Tc < point
-
-// unit test lingo
-// fake: is a generic term used to describe either a stub or a mock
-// mock: is a fake object in the system that decides wether or not a unit test has passed or failed
-// stub: is a controllable replacement for an existing dependency in the system
-
-// a. Avoid infrastructure dependencies
-// b. Name:
-//     1. method being tested
-//     2. scenario in which it's being tested
-//     3.expected behaviour
-// c. Structure:
-//     1. arrange objects
-//     2. act on an object
-//     3. assert
-// d. Write minimally passing tests, the input used should be the simplest possible to verify the behaviour we are currently testing
-// e. Avoid magic strings
-// f. Avoid logic inside tests
-// g. Avoid multiple acts (assertions), only one per test
-// h. Validate private methods by testing public methods, since private methods will eventually be called by public methods it is a better practice to make sure that this public methods are using the private methods correctly
-// i. Sometimes we will have to write our code in a way that is testable, i.e. introduce parts just for testing purposes and other things
